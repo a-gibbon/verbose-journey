72765e94f3e10c12487ac86979b167e5f481e928
a_gibbon <a-gibbon@users.noreply.github.com>
2018-01-16 20:38:36 +1100

    Create Disable-Windows-Event-Logs.ps1

---
+++ powershell/Disable-Windows-Event-Logs.ps1
@@ -0,0 +1,31 @@
+﻿Function Disable-All-WinEvents {
+
+    Begin {
+        $AllLogs = Get-WinEvent -ListLog * -Force -ErrorAction SilentlyContinue
+    }
+
+    Process {
+
+        $AllLogs | % -Process {
+            if ($_) {
+                if ($_.get_IsClassicLog()) {
+                    Write-Warning -Message "[!] Cannot Disable Classic Log $($_.LogName)"
+                } else {
+                    try {
+                        if ($_.get_IsEnabled()) {
+                            $_.set_IsEnabled($False)
+                            $_.SaveChanges() | Out-Null
+                            Write-Host "[-] Successfully Disabled: $($_.LogName)"
+                        } else {
+                            Write-Host "[x] Already Disabled: $($_.LogName)"
+                        }
+                    } catch {
+                        Write-Warning -Message "[!] Failed to Disable $($_.LogName) Due to Error $($_.Exception.Message)"
+                    }
+                }
+            } else {
+                Write-Warning -Message "[!] Cannot Find: $($_.LogName)"
+            }
+        }
+    }
+}
